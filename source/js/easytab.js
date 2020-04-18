5c9dd8150ef87145086dd45d45350df5225b6b1d
BabakFP <44144724+babakfp@users.noreply.github.com>
2020-04-18 07:25:17 -0700

    Update easytab.js
    better commenting

--- source/js/easytab.js
+++ source/js/easytab.js
@@ -1,26 +1,26 @@
 
-// Start -> Turn on the music
+// Start -> Turn on the music ~~(^_^)~~
 
 const easytab = ( ContainerID, active ) => {
     
-    // storing, main container that user creates with easytab() function, inside easytabContainer Variable
+    // Get ContainerID - Tab navigation Unique container ID
     let easytabContainer = document.getElementById(ContainerID)
 
 
-    // Tab's wrapper
+    // Tab's wrapper - All Tab's are located inside this class
     let tabList = easytabContainer.querySelector('.easyatb-tab-list')
 
-    // Get all Tab's (from Tab's wrapper)
+    // Get all Tab's avalible inside tabList
     let tabArray = tabList.querySelectorAll('.easyatb-tab')
-
 
     // Number of avalible Tab's
     let tabLength = tabArray.length
+    
 
-    // Content's wrapper
+    // Content's wrapper - All Content's are located inside this class
     let contentList = easytabContainer.querySelector('.easyatb-content-list')
     
-    // Get all Content's (from Content's wrapper)
+    // Get all Content's avalible inside contentList
     let contentArray = contentList.querySelectorAll('.easytab-content')
 
 
@@ -42,7 +42,7 @@ const easytab = ( ContainerID, active ) => {
             
         }
         
-        // If type is Number - Active index = Number
+        // If type of 'active' is Number - Active index = 'active' value
         else if ( typeof(active) ) {
 
             tabArray[active - 1].classList.add('active')
@@ -55,7 +55,7 @@ const easytab = ( ContainerID, active ) => {
         // Event -> When each Tab clicked
         tab.addEventListener('click', () => {
 
-            // Make every Tab and Content, unactive
+            // Remove 'active' class from all Tab's & Content's. Make All active Tab's & Content's, unactive
             for ( let i = 0; i < tabLength; i++ ) {
 
                 tabArray[i].classList.remove('active')
@@ -64,10 +64,10 @@ const easytab = ( ContainerID, active ) => {
 
             }
 
-            // Make clicked Tab, active
+            // Add class 'active' to target (clicked Tab). Make target active
             tab.classList.add('active')
 
-            // Make clicked Tab Content, active
+            // Add class 'active' to target (clicked Content). Make target active
             content.classList.add('active')
    
         })
