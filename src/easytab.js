1e9f21cf99517d42ecc3fba7f8d1eff8f4d1ca64
babakfp <babak.bxf@gmail.com>
2020-10-05 18:35:05 -0700

    changed to reference to element by it self

--- src/easytab.js
+++ src/easytab.js
@@ -1,57 +1,41 @@
-/*!
-https://github.com/babakfp/easy-tab
-*/
-
-
 const easyTab = function(container, active){
-    
-    // wrapp's all
-    container = document.getElementById(container)
+  
+  // tabs
+  let tabs = container.querySelectorAll('.easytab-tab')
+  let tabsLength = tabs.length
+  
+  // contents
+  let contents = container.querySelectorAll('.easytab-content')
 
-    // tabs
-    let tabs = container.querySelectorAll('.easytab-tab')
-    let tabsLength = tabs.length
-    
-    // contents
-    let contents = container.querySelectorAll('.easytab-content')
+  // activate first tab
+  if (active == undefined) {
+    tabs[0].classList.add('active')
+    contents[0].classList.add('active')
 
+  // activate tab that active prop contains
+  } else if (typeof active) {
+    tabs[active-1].classList.add('active')
+    contents[active-1].classList.add('active')
+  }
 
-    // activate first tab
-    if (active == undefined)
-    {
-        tabs[0].classList.add('active')
-        contents[0].classList.add('active')
-    }
-    
-    // activate tab that active prop contains
-    else if (typeof(active))
-    {
-        tabs[active-1].classList.add('active')
-        contents[active-1].classList.add('active')
-    }
+  for (let i = 0; i < tabsLength; i++) {
+    let tab = tabs[i]
+    let content = contents[i]
 
+    // each tab clicked
+    tab.addEventListener('click', _=> {
 
-    for (let i = 0; i < tabsLength; i++)
-    {
-        let tab = tabs[i]
-        let content = contents[i]
+      // this will remove "active" class from tabs and contents
+      for (let i = 0; i < tabsLength; i++) {
+        tabs[i].classList.remove('active')
+        contents[i].classList.remove('active')
+      }
 
-        // each tab clicked
-        tab.addEventListener('click', function()
-        {
-            // this will remove "active" class from tabs and contents
-            for (let i = 0; i < tabsLength; i++)
-            {
-                tabs[i].classList.remove('active')
-                contents[i].classList.remove('active')
-            }
-
-            // add class "active" to clicked tab
-            tab.classList.add('active')
-
-            // add class "active" to clicked tab content
-            content.classList.add('active')
-        })
-    }
+      // add class "active" to clicked tab
+      tab.classList.add('active')
+      // add class "active" to clicked tab content
+      content.classList.add('active')
+    })
+  }
 
 }
