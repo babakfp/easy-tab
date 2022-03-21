9b38bb3aab2ae30b0a032c7593b8e07b72a0b298
babakfp <44144724+babakfp@users.noreply.github.com>
2022-03-21 13:39:26 -0700

    rewrite to class EasyTab

--- README.md
+++ README.md
@@ -1,24 +1,39 @@
-# easy-tab
+# Easy Tab
 
-## Introdation
-[Easy Tab](https://github.com/babakfp/easy-tab) Navigation is a simple library to easily create tab navigation's.
+Easily create tab navigations.
 
-- container (element that contain tabs and contents), need to have a unique `id` to library be able to select it.
-- All necessary class'es starts with the `easytab` word and after that, `-` character.
-- To getting started, you need to include `easytab.min.css` and `easytab.min.js` files into your project.
-- You need to write code `easyTab('container-unique-id')` after loading library code js file.
-- You can have ultimate tab navigation on the same page. you need to give a unique `id` to each tab navigation container and then call that id with this code `easyTab('container-unique-id')`.
-- If you what to tab number 2 activate in first, do this `easyTab('container-unique-id', 2)`. just replace tab index you wish.
-- If you like this, please press like button.
+## Production files
 
-## Files
-- easytab.min.js
-- easytab.min.css
+Path: `/dist`
 
-### Author
-Author name: babakfp
-- Contact: [Telegram](https://t.me/babakfp)
-- Website [visit](http://babakfp.ir)
-- Github [visit](https://github.com/babakfp)
-- Codepen [visit](https://codepen.io/babakfp)
-- Stack Overflow [visit](https://stackoverflow.com/users/10799492/babakfp)
+- `/dist/easytab-dist.js`
+- `/dist/easytab-dist.css`
+
+## Usage
+
+```js
+	new EasyTab(container, activeIndex = 1)
+```
+
+```html
+<div class="my-easytab">
+	<li class="easytab__tab">Tab 1</li>
+	<li class="easytab__tab">Tab 2</li>
+
+	<div class="easytab__panel">Content 1</div>
+	<div class="easytab__panel">Content 2</div>
+</div>
+
+<script>
+	new EasyTab('.my-easytab')
+</script>
+
+<!-- Multiple instances with one line of code -->
+
+<script>
+	const allEasytabs = document.querySelectorAll('.my-easytab')
+	allEasytabs.forEach(el => {
+		new EasyTab(el)
+	})
+</script>
+```
--- src/easytab.js
+++ src/easytab.js
@@ -1,48 +1,88 @@
-const easyTab = function(container, active){
-  
-  if (!container) {
-    console.error(`Error:
-In your project, the wrapper of easyTab() couldn't found!.
-    `)
-    return
-  }
+class EasyTab
+{
+	/**
+	 * Class constructor will receive the injections as parameters.
+	 */
+	constructor (container, activeIndex = 1)
+	{
+		this.container = container
+		this.activeIndex = activeIndex - 1
 
-  // tabs
-  let tabs = container.querySelectorAll('[easytab-tab]')
-  let tabsLength = tabs.length
-  
-  // contents
-  let contents = container.querySelectorAll('[easytab-content]')
+		this.containerAsElement
+		this.tabsAsElementList
+		this.panelsAsElementList
 
-  // activate first tab
-  if (active == undefined) {
-    tabs[0].classList.add('active')
-    contents[0].classList.add('active')
+		this.itemsLength
 
-  // activate tab that active prop contains
-  } else if (typeof active) {
-    tabs[active-1].classList.add('active')
-    contents[active-1].classList.add('active')
-  }
+		// Log an console error if the first argument of EasyTab class were missing
+		if (!this.container) {
+			console.error(`The first argument of EasyTab class is missing!`)
+			return
+		}
 
-  for (let i = 0; i < tabsLength; i++) {
-    let tab = tabs[i]
-    let content = contents[i]
+		// Get DOM Element from query selector
+		if (typeof this.container === 'string') {
+			this.containerAsElement = document.querySelector(this.container)
+		} else {
+			this.containerAsElement = this.container
+		}
 
-    // each tab clicked
-    tab.addEventListener('click', _=> {
+		// Add a default class name to container element
+		this.containerAsElement.classList.add('easytab')
 
-      // this will remove "active" class from tabs and contents
-      for (let i = 0; i < tabsLength; i++) {
-        tabs[i].classList.remove('active')
-        contents[i].classList.remove('active')
-      }
+		// Get tabs and panels as DOM element
+		this.tabsAsElementList = this.containerAsElement.querySelectorAll('.easytab__tab')
+		this.panelsAsElementList = this.containerAsElement.querySelectorAll('.easytab__panel')
 
-      // add class "active" to clicked tab
-      tab.classList.add('active')
-      // add class "active" to clicked tab content
-      content.classList.add('active')
-    })
-  }
+		// Check is tabs and panels available
+		if (this.tabsAsElementList.length === 0) {
+			console.error(`No tabs (.easytab__tab) found in EasyTab!`)
+			return
+		}
+		if (this.panelsAsElementList.length === 0) {
+			console.error(`No panels (.easytab__panel) found in EasyTab!`)
+			return
+		}
+		
+		// Check is tabs and panels length is the same
+		if (this.tabsAsElementList.length !== this.panelsAsElementList.length) {
+			console.error(`The length of tabs and panels in EasyTab need to be equal!`)
+			return
+		}
 
+		// Set items length. Just to make it simple
+		this.itemsLength = this.tabsAsElementList.length
+
+		// Add `active` class to selected tab and panel index
+		this.tabsAsElementList[this.activeIndex].classList.add('active')
+    this.panelsAsElementList[this.activeIndex].classList.add('active')
+
+		// Loop
+		for (let i = 0; i < this.itemsLength; i++) {
+			let tab = this.tabsAsElementList[i]
+			let panel = this.panelsAsElementList[i]
+	
+			// each tab clicked
+			tab.addEventListener('click', _=> {
+
+				// To not readd classes if clicking on active tab
+				if (tab.classList.contains('active')) {
+					return
+				}
+	
+				// this will remove "active" class from tabs and contents
+				for (let i = 0; i < this.itemsLength; i++) {
+					this.tabsAsElementList[i].classList.remove('active')
+					this.panelsAsElementList[i].classList.remove('active')
+				}
+	
+				// add class "active" to clicked tab
+				tab.classList.add('active')
+				// add class "active" to clicked tab panel
+				panel.classList.add('active')
+			})
+		}
+
+	}
+	
 }
